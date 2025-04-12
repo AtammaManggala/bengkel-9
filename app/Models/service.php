1dafa5080733ee7ecd884b1c35b75b0246da102f
Adelleraa <adeliashabiraaaputri4450@gmail.com>
2025-04-12 14:33:20 +0700

    Menambah model service

--- app/Models/service.php
+++ app/Models/service.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class service extends Model
 {
     use HasFactory;
+
+    protected $table = 'service';
+    protected $primaryKey = 'id_service';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_kendaraan',
+        'keluhan',
+        'id_mekanik',
+        'tgl_masuk',
+        'tgl_keluar',
+        'id_jns_service',
+    ];
+
+    public function kendaraan()
+    {
+        return $this->belongsTo(Kendaraan::class, 'id_kendaraan');
+    }
+
+    public function mekanik()
+    {
+        return $this->belongsTo(Mekanik::class, 'id_mekanik');
+    }
+
+    public function jenis_service()
+    {
+        return $this->belongsTo(jnsService::class, 'id_jns_service');
+    }
+
+    public function detail_services()
+    {
+        return $this->hasMany(Service::class, 'id_service');
+    }
 }
