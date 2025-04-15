e39f79bbe0b6d2e0b7397155c365d5234e32808f
Adelleraa <adeliashabiraaaputri4450@gmail.com>
2025-04-15 15:00:42 +0700

    "Menambahkan routes pemilik dan kendaraan"

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\JnsKendaraancontroller;
 use App\Http\Controllers\kendaraancontroller;
 use App\Models\jnsKendaraan;
+use App\Models\kendaraan;
+use App\Models\pemilik;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,10 +30,19 @@ route::get('/jnsKendaraan/edit/{id}', [JnsKendaraancontroller::class, 'edit'])->
 route::post('/jnsKendaraan/edit/{id}', [JnsKendaraancontroller::class, 'update'])->name('jnskendaraan.update');
 route::get('/jnsKendaraan/delete/{id}', [JnsKendaraancontroller::class, 'destroy'])->name('jnskendaraan.delete');
 
-//kendaraan
-route::get('/kendaraan', [kendaraancontroller::class, 'index'])->name('kendaraan.index');
-route::get('/kendaraan', [kendaraancontroller::class, 'create'])->name('kendaraan.create');
-route::post('/kendaraan', [kendaraancontroller::class, 'store'])->name('kendaraan.store');
+///route kendaraan
+Route::get('/Kendaraan', [kendaraan::class, 'index'])->name('Kendaraan.index');
+Route::get('/Kendaraan/add', [Kendaraan::class, 'create'])->name('Kendaraan.create');
+Route::post('/Kendaraan/add', [Kendaraan::class, 'store'])->name('Kendaraan.store');
+Route::get('/Kendaraan/edit/{id_kendaraan}', [Kendaraan::class, 'edit'])->name('Kendaraan.edit');
+Route::post('/Kendaraan/update/{id_kendaraan}', [Kendaraan::class, 'update'])->name('Kendaraan.update');
+Route::get('/Kendaraan/delete/{id_kendaraan}', [Kendaraan::class, 'destroy'])->name('Kendaraan.delete');
 
-
+//route pemilik
+Route::get('/pemilik', [pemilik::class, 'index'])->name('Pemilik.index');
+Route::get('/pemilik/add', [pemilik::class, 'create'])->name('Pemilik.create');
+Route::post('/pemilik/add', [pemilik::class, 'store'])->name('Pemilik.store');
+Route::get('/pemilik/edit/{id_pemilik}', [pemilik::class, 'edit'])->name('Pemilik.edit');
+Route::post('/pemilik/update/{id_pemilik}', [pemilik::class, 'update'])->name('Pemilik.update');
+Route::get('/pemilik/delete/{id_pemilik}', [pemilik::class, 'destroy'])->name('Pemilik.delete');
 
