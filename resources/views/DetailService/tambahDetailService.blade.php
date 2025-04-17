d6b4828bbfe061df8d7d6d8b84a90bf1a4abc408
AtammaManggala <atammamanggala05@gmail.com>
2025-04-17 13:49:55 +0700

    upload template DetailService

--- resources/views/DetailService/tambahDetailService.blade.php
+++ resources/views/DetailService/tambahDetailService.blade.php
@@ -1,10 +1,91 @@
-<form action="" method="post" name="tambah">
-    @csrf 
-    <label for="">Sparepart : </label>
-    <input type="text" name="sparepart" id=""><br>
-    
-    <label for="">Harga : </label>
-    <input type="text" name="harga" id=""><br>
+@include('layout.header')
+    <!--begin::App Wrapper-->
+    <div class="app-wrapper">
+      <!--begin::Header-->
+      @include('layout.navbar')
+      
+      <!--end::Header-->
+      <!--begin::Sidebar-->
+      @include('layout.sidebar')
+      
+      <!--end::Sidebar-->
+      <!--begin::App Main-->
+      <main class="app-main">
 
-    <input type="submit" name="simpan" value="simpan"><br>
-</form>
+      <div class="app-content-header">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-sm-6"><h3 class="mb-0">Tambah Detail Service</h3></div>
+              <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-end">
+                  <li class="breadcrumb-item"><a href="#">Home</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">Tambah Detail Service</li>
+                </ol>
+              </div>
+            </div>
+            <!--end::Row-->
+          </div>
+          <!--end::Container-->
+        </div>
+
+
+        <div class="app-content">
+        <!--begin::Container-->
+            <div class="container-fluid">
+                    <!--begin::Row-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="card card-primary card-outline mb-4">
+                                <!--begin::Header-->
+                                <div class="card-header"><div class="card-title">Silahkan isi data</div></div>
+                                <!--end::Header-->
+                                <!--begin::Form-->
+                                <form action="" method="post" name="tambah">
+                                @csrf
+                                <!--begin::Body-->
+                                <div class="card-body">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Sparepart</label>
+                                        <input type="text" class="form-control"  name="sparepart"/><br>
+
+                                        <label for="" class="form-label">Harga</label>
+                                        <input type="text" class="form-control"  name="harga"/><br>
+
+                                    </div>
+                                </div>
+                                <!--end::Body-->
+                                <!--begin::Footer-->
+                                <div class="card-footer">
+                                    <button type="submit" name="simpan"  class="btn btn-primary">simpan bro</button>
+                                </div>
+                                <!--end::Footer-->
+                                </form>
+                            <!--end::Form-->
+                            </div>
+
+
+
+
+
+                        </div>
+                    </div>
+            </div>
+        </div>
+
+        <div class="app-content">
+          <!--begin::Container-->
+        
+</section>
+</main>
+      <!--end::App Main-->
+      <!--begin::Footer-->
+      @include('layout.coba')
+      
+
+      <!--end::Footer-->
+    </div>
+    @include('layout.footer')
+
+
--- resources/views/DetailService/tampilDetailService.blade.php
+++ resources/views/DetailService/tampilDetailService.blade.php
@@ -1,26 +1,102 @@
-<a href="{{route('detailService.create')}}">
-    <input type="button" value="Tambah">
-</a>
+@include('layout.header')
+    <!--begin::App Wrapper-->
+    <div class="app-wrapper">
+      <!--begin::Header-->
+      @include('layout.navbar')
+      
+      <!--end::Header-->
+      <!--begin::Sidebar-->
+      @include('layout.sidebar')
+      
+      <!--end::Sidebar-->
+      <!--begin::App Main-->
+      <main class="app-main">
 
-<table border='1'>
-    <tr>
-            <td>No</td>
-            <td>Sparepart</td>
-            <td>Harga</td>
-            <td>Aksi</td>
-    </tr>
-    @php $no = 1; @endphp
-    @foreach($data as $row)
+      <div class="app-content-header">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-sm-6"><h3 class="mb-0">Detail Service</h3></div>
+              <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-end">
+                  <li class="breadcrumb-item"><a href="#">Home</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                </ol>
+              </div>
+            </div>
+            <!--end::Row-->
+          </div>
+          <!--end::Container-->
+        </div>
 
-    <tr>
-        <td>{{$no++}}</td>
-         <td>{{$row->sparepart}}</td>
-         <td>{{$row->harga}}</td>
-         <td>
-            <a href="{{route('detailService.edit',$row->id_detail_service)}}">update</a>  
-            |  
-            <a href="{{route('detailService.delete',$row->id_detail_service)}}">delete</a>
-        </td>
-    </tr>
-    @endforeach
-</table>
+
+        <div class="app-content">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-md-12">
+                <div class="card mb-4">
+                    <div class="card-header">
+                      <div class="row">
+                          <div class="col-11">
+                            <h3 class="card-title">Data Keseluruhan Detail Service</h3>
+                          </div>
+                          <div class="col-1">
+                            <a href="{{route('detailService.create')}}">
+                              <input type="button" value="Tambah" class="btn btn-success">
+                            </a>  
+                          </div>
+                      </div>
+                    </div>
+
+                    <div class="card-body p-0">
+                        <table class="table table-striped">
+                            <tr>
+                                    <td>No</td>
+                                    <td>Sparepart</td>
+                                    <td>Harga</td>
+                                    <td>Aksi</td>
+                            </tr>
+                            @php $no = 1; @endphp
+                            @foreach($data as $row)
+
+                            <tr>
+                                <td>{{$no++}}</td>
+                                <td>{{$row->sparepart}}</td>
+                                <td>{{$row->harga}}</td>
+                                <td>
+                                    <a href="{{route('detailService.edit',$row->id_detail_service)}}"
+                                    class="btn btn-warning">update</a>  
+                                    |  
+                                    <a href="{{route('detailService.delete',$row->id_detail_service)}}" 
+                                    class="btn btn-danger">delete</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="app-content">
+          <!--begin::Container-->
+          <div class="container-fluid">
+
+          </div>
+</section>
+</main>
+      <!--end::App Main-->
+      <!--begin::Footer-->
+      @include('layout.coba')
+      
+
+      <!--end::Footer-->
+    </div>
+    @include('layout.footer')
+
+
