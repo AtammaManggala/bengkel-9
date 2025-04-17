248e39ec39fea01064ba4c0918f23aca52c27279
AtammaManggala <atammamanggala05@gmail.com>
2025-04-17 14:37:04 +0700

    upload template mekanik

--- resources/views/mekanik/updateMekanik.blade.php
+++ resources/views/mekanik/updateMekanik.blade.php
@@ -1,21 +1,101 @@
-@foreach($data as $row)
-<form action="{{route('mekanik.update',$id)}}" method="post" name="tambah">
-    @csrf 
-    <label for="">Nama Mekanik : </label>
-    <input type="text" name="mekanik" id="" value="{{$row->nm_mekanik}}"><br>
+@include('layout.header')
+    <!--begin::App Wrapper-->
+    <div class="app-wrapper">
+      <!--begin::Header-->
+      @include('layout.navbar')
+      
+      <!--end::Header-->
+      <!--begin::Sidebar-->
+      @include('layout.sidebar')
+      
+      <!--end::Sidebar-->
+      <!--begin::App Main-->
+      <main class="app-main">
+
+      <div class="app-content-header">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-sm-6"><h3 class="mb-0">Tambah Mekanik</h3></div>
+              <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-end">
+                  <li class="breadcrumb-item"><a href="#">Home</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">Tambah mekanik</li>
+                </ol>
+              </div>
+            </div>
+            <!--end::Row-->
+          </div>
+          <!--end::Container-->
+        </div>
+
+
+        <div class="app-content">
+        <!--begin::Container-->
+            <div class="container-fluid">
+                    <!--begin::Row-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="card card-primary card-outline mb-4">
+                                <!--begin::Header-->
+                                <div class="card-header"><div class="card-title">Silahkan isi data</div></div>
+                                <!--end::Header-->
+                                <!--begin::Form-->
+                                @foreach($data as $row)
+                                <form action="{{route('mekanik.update',$id)}}" method="post" name="tambah">
+                                @csrf
+                                <!--begin::Body-->
+                                <div class="card-body">
+                                    <div class="mb-3">
+                                        <label for="" class="form-label">Nama Mekanik</label>
+                                        <input type="text" class="form-control"  name="mekanik" value="{{$row->nm_mekanik}}"/><br>
+
+                                        <label for="" class="form-label">Tanggal Lahir</label>
+                                        <input type="text" class="form-control"  name="tgl_lahir" value="{{$row->tgl_lahir}}"/><br>
+
+                                        <label for="" class="form-label">Alamat</label>
+                                        <input type="text" class="form-control"  name="alamat" value="{{$row->alamat}}"/><br>
+
+                                        <label for="" class="form-label">Nomor Induk Kependudukan</label>
+                                        <input type="text" class="form-control"  name="nik" value="{{$row->nik}}"/><br>
+
+                                        <label for="" class="form-label">Nomor Handphone</label>
+                                        <input type="text" class="form-control"  name="no_hp" value="{{$row->no_hp}}"/><br>
+
+                                    </div>
+                                </div>
+                                <!--end::Body-->
+                                <!--begin::Footer-->
+                                <div class="card-footer">
+                                    <button type="submit" name="simpan"  class="btn btn-primary">simpan bro</button>
+                                </div>
+                                <!--end::Footer-->
+                                </form>
+                                @endforeach
+                            <!--end::Form-->
+                            </div>
+
+
+
+
+
+                        </div>
+                    </div>
+            </div>
+        </div>
+
+        <div class="app-content">
+          <!--begin::Container-->
+        
+</section>
+</main>
+      <!--end::App Main-->
+      <!--begin::Footer-->
+      @include('layout.coba')
+      
+
+      <!--end::Footer-->
+    </div>
+    @include('layout.footer')
     
-    <label for="">Tanggal Lahir : </label>
-    <input type="text" name="tgl_lahir" id="" value="{{$row->tgl_lahir}}"><br>
-
-    <label for="">Alamat : </label>
-    <input type="text" name="alamat" id="" value="{{$row->alamat}}"><br>
-
-    <label for="">Nomor Induk Kependudukan : </label>
-    <input type="text" name="nik" id="" value="{{$row->nik}}"><br>
-
-    <label for="">Nomor Handphone : </label>
-    <input type="text" name="no_hp" id="" value="{{$row->no_hp}}"><br>
-
-    <input type="submit" name="simpan" value="simpan">
-</form>
-@endforeach
